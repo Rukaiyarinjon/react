826d027840325bd3afb2ee3df2024cb6859e4dde
Rukaiyarinjon <info.rukaiyarinjon@gmail.com>
2025-01-14 12:40:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Navber from './components/navber/Navber';
+import Card from './components/card/Card';
+import Table from './components/table/table';
+
+
+function App() {
+
+
+  return (
+    <>
+       <Navber></Navber>
+       <Card></Card>
+       
+       <Table></Table>
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+
+
+const Card = () => {
+    return (
+        <div className="card" style={{ width: '18rem' }}>
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <p className="card-text text-danger">
+          Some quick example text to build on the card title and make up the bulk of the  content.
+        </p>
+      </div>
+    </div>
+    );
+};
+
+export default Card;
